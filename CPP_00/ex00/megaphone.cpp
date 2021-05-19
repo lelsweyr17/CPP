9173d57b8004536935557a2b6d003606125b7e69
Lippman Elsweyr <lelsweyr@un-h2.kzn.21-school.ru>
2021-05-19 21:23:12 +0300

    CPP_00

---
+++ CPP_00/ex00/megaphone.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+    if (argc == 1) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    }
+    else {
+        for (int i = 1; i < argc; i++)
+        {
+            std::string buf = argv[i];
+            for (int j = 0; j < strlen(argv[i]); j++)
+                std::cout << (char)toupper(buf[j]);
+            std::cout << " ";
+        }
+        std::cout << std::endl;
+    }
+    return (0);
+}
