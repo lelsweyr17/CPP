67557d18b0b92d117f59960e41f1431ff071ca71
Lippman Elsweyr <lelsweyr@pr-a1.kzn.21-school.ru>
2021-06-06 18:31:07 +0300

    add ex00, ex01

---
+++ CPP_07/ex01/iter.hpp
@@ -0,0 +1,18 @@
+#ifndef ITER_HPP
+# define ITER_HPP
+
+#include <iostream>
+
+template <typename T> void f(T &a) {
+	std::cout << a << "!!!" << std::endl;
+}
+
+template <typename T> void iter(T *a, unsigned int b, void (*f)(T&)) {
+	for (int i = 0; i < b; i++) {
+		(*f)(a[i]);
+	}
+	std::cout << std::endl;
+}
+
+
+#endif 
---
+++ CPP_07/ex01/main.cpp
@@ -0,0 +1,16 @@
+#include "iter.hpp"
+
+int main() {
+	double array1[] = {123.234, 456456.2, 77.777, 450.00};
+	::iter(array1, 4, f);
+	
+	std::string	array2[] = {"abc", "def", "ghi", "jkl", "mno"};
+	::iter(array2, 5, f);
+
+	int array3[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	::iter(array3, 9, f);
+
+	char array4[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+	::iter(array4, 26, f);
+	return 0;
+}
