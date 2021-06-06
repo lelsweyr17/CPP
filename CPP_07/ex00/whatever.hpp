7fbfe3380481e5263b3e102352de4f74c344f228
Lippman Elsweyr <lelsweyr@pr-a1.kzn.21-school.ru>
2021-06-06 21:29:33 +0300

    ref ex00 ex01

--- CPP_07/ex00/whatever.hpp
+++ CPP_07/ex00/whatever.hpp
@@ -3,17 +3,20 @@
 
 #include <iostream>
 
-template <typename T> void swap(T &a, T &b) {
+template <typename T>
+void swap(T &a, T &b) {
 	T tmp = b;
 	b = a;
 	a = tmp;
 }
 
-template <typename T> T	min(T a, T b) {
+template <typename T>
+T	min(T a, T b) {
 	return (a < b) ? a : b;
 }
 
-template <typename T> T	max(T a, T b) {
+template <typename T>
+T	max(T a, T b) {
 	return (a > b) ? a : b;
 }
 
--- CPP_07/ex01/iter.hpp
+++ CPP_07/ex01/iter.hpp
@@ -3,16 +3,17 @@
 
 #include <iostream>
 
-template <typename T> void f(T &a) {
+template <typename T>
+void	f(T &a) {
 	std::cout << a << "!!!" << std::endl;
 }
 
-template <typename T> void iter(T *a, unsigned int b, void (*f)(T&)) {
+template <typename T>
+void	iter(T *a, unsigned int b, void (*f)(T&)) {
 	for (int i = 0; i < b; i++) {
 		(*f)(a[i]);
 	}
 	std::cout << std::endl;
 }
-
 
 #endif 
